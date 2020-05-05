fb72723f842c8d51a6a1dd50136be1e92a4837fa
Johnson <wj_johnson9581314@163.com>
2020-05-05 15:28:06 +0800

    goodlist 组件

---
+++ src/components/GoodList/GoodList.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './GoodList.scss';
+
+function GoodList() {
+    return (
+        <div className="goodList">
+            <div className="items">
+                <span className="goodList-image">
+                    {/* <img src="" alt=""/> */}
+                </span>
+                <span className="goodList-goodTitle">
+                    Apple/苹果2019新款AirPods Pro真无线耳机入耳式蓝牙降噪充电盒
+                </span>
+                <span className="goodList-price-buy">
+                    <span className="price">¥1,899</span>
+                    <span className="buy">8036人已购买</span>
+                </span>
+            </div>
+            <div className="items">
+                <span className="goodList-image">
+                    {/* <img src="" alt=""/> */}
+                </span>
+                <span className="goodList-goodTitle">
+                    Apple/苹果2019新款AirPods Pro真无线耳机入耳式蓝牙降噪充电盒
+                </span>
+                <span className="goodList-price-buy">
+                    <span className="price">¥1,899</span>
+                    <span className="buy">8036人已购买</span>
+                </span>
+            </div>
+            <div className="items">
+                <span className="goodList-image">
+                    {/* <img src="" alt=""/> */}
+                </span>
+                <span className="goodList-goodTitle">
+                    Apple/苹果2019新款AirPods Pro真无线耳机入耳式蓝牙降噪充电盒
+                </span>
+                <span className="goodList-price-buy">
+                    <span className="price">¥1,899</span>
+                    <span className="buy">8036人已购买</span>
+                </span>
+            </div>
+            <div className="items">
+                <span className="goodList-image">
+                    {/* <img src="" alt=""/> */}
+                </span>
+                <span className="goodList-goodTitle">
+                    Apple/苹果2019新款AirPods Pro真无线耳机入耳式蓝牙降噪充电盒
+                </span>
+                <span className="goodList-price-buy">
+                    <span className="price">¥1,899</span>
+                    <span className="buy">8036人已购买</span>
+                </span>
+            </div>
+        </div>
+    )
+}
+export default GoodList;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,8 @@
 import React, { PureComponent } from "react";
 import SearchHeader from "../../components/SearchHeader/SearchHeader";
 import Banner from "../../components/Banner/Banner";
-import NavBar from '../../components/NavBar/NavBar'
+import NavBar from '../../components/NavBar/NavBar';
+import GoodList from '../../components/GoodList/GoodList';
 export default class Home extends PureComponent {
   render() {
     return (
@@ -11,6 +12,7 @@ export default class Home extends PureComponent {
           <div className="banner-container">
             <Banner />
             <NavBar />
+            <GoodList />
           </div>
         </div>
       </>
